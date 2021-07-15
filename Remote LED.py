e1a266100be793a36b7cfca2cffe32a905bb08d7
Yeming Chen <87426521+Yeming777@users.noreply.github.com>
2021-07-15 21:30:02 +1000

    Update Remote LED.py

--- Remote LED.py
+++ Remote LED.py
@@ -9,14 +9,14 @@ GPIO.setup(18,GPIO.OUT)
 #get the key and url from thingspeak
 KEY='KEY' url='url' params = {'key': KEY}
 while True:
-  #read data from thingspeak and reshape it to get the useable data
-  res = requests.get(url, params=params).json() field_1=res['feeds']
-  #collect the last input data
-  index=len(field_1)-1
-  temp=field_1[index]
-  #based on the last entry data to determain the on/off of the LED
-  i=temp['field1'] print(i)
-  if i=='0':
-    GPIO.output(18,GPIO.LOW) 
-  else:
-    GPIO.output(18,GPIO.HIGH)
+    #read data from thingspeak and reshape it to get the useable data
+    res = requests.get(url, params=params).json() field_1=res['feeds']
+    #collect the last input data
+    index=len(field_1)-1
+    temp=field_1[index]
+    #based on the last entry data to determain the on/off of the LED
+    i=temp['field1'] print(i)
+    if i=='0':
+      GPIO.output(18,GPIO.LOW) 
+    else:
+      GPIO.output(18,GPIO.HIGH)
